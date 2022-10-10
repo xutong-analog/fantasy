b8bef176e60aef5d281dab8d9912effc78988973
xutong-analog <114386539+xutong-analog@users.noreply.github.com>
2022-10-10 21:33:52 +0800

    Create AnalogysemiCSD.c

---
+++ Power Monitor by AnalogySemi/AnalogysemiCSD.c
@@ -0,0 +1,22 @@
+#include "AnalogysemiCSD.h"
+/*Initial CSD203*/
+void CSD203_Init(CSD_CONFIG *CSD203_CFG)
+{
+	uint16_t Data=0,ADDR=0;
+	Data|=(CSD203_CFG->RST)<<15;
+	Data|=(CSD203_CFG->Average)<<9;
+	Data|=(CSD203_CFG->VBUS_Conv_Time)<<5;
+	Data|=(CSD203_CFG->VShunt_Conv_Time)<<2;
+	Data|=CSD203_CFG->Mode;
+	ADDR=(CSD203_CFG->DeviceADDR);
+	IIC_DUT_W(ADDR,CONFIGURATION,Data);
+	printf("%x\r\n",Data);
+}
+/*Read Vbus*/
+uint16_t CSD203_ReadVbus(CSD_CONFIG *CSD203_CFG)
+{
+	uint16_t Data=0,ADDR=0;
+	ADDR=(CSD203_CFG->DeviceADDR);
+	Data=IIC_DUT_R(ADDR,BUS_VOLTAGE);
+	return Data;
+}
