2d2ce11dc8a918c9f9c110374fc29c357907f00d
xutong-analog <114386539+xutong-analog@users.noreply.github.com>
2022-10-10 11:57:13 +0800

    Create nalogysemiCSD.h

---
+++ Power Monitor by AnalogySemi/nalogysemiCSD.h
@@ -0,0 +1,17 @@
+#ifndef AnalogysemiCSD_H
+#define AnalogysemiCSD_H
+/* CSD203 regsistor map */
+#define CONFIGURATION   0X00
+#define SHUNT_VOLTAGE   0X01
+#define BUS_VOLTAGE     0X02
+#define POWER           0X03
+#define CURRENT         0x04
+#define CALIBRATION     0x05
+#define MASKENABLE      0x06
+#define ALERTLIMIT      0x07
+/*
+#define MANUFACTURER 0x00
+#define DIE_ID 0x00
+*/
+
+#endif 
