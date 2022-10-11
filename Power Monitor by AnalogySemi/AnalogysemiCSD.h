c5041baa094cdca6d45bb8a9450572a44af7d320
xutong-analog <114386539+xutong-analog@users.noreply.github.com>
2022-10-11 11:00:46 +0800

    Update AnalogysemiCSD.h

--- Power Monitor by AnalogySemi/AnalogysemiCSD.h
+++ Power Monitor by AnalogySemi/AnalogysemiCSD.h
@@ -5,6 +5,8 @@
 #include "stdio.h"
 #include "IIC.h"
 
+
+
 /* CSD203 Regsistor map */
 #define CONFIGURATION   0X00
 /*Read Only*/
@@ -16,6 +18,26 @@
 #define CALIBRATION     0x05
 #define MASKENABLE      0x06
 #define ALERTLIMIT      0x07
+
+
+/*Calibration Calculation parameter*/
+/* This Parameter Gain*10000K*/
+#define CalParam        51200
+/* ↓ add your shunt here ↓   */
+#define CSD_CONFIG_Rs_1mR						1
+#define CSD_CONFIG_Rs_2mR						2
+#define CSD_CONFIG_Rs_5mR						5
+#define CSD_CONFIG_Rs_10mR					10
+#define CSD_CONFIG_Rs_20mR					20
+#define CSD_CONFIG_Rs_50mR					50
+#define CSD_CONFIG_CurrentLsb1mA   	10
+#define CSD_CONFIG_CurrentLsb2mA   	20
+#define CSD_CONFIG_CurrentLsb5mA   	50
+#define CSD_CONFIG_CurrentLsb10mA  	100
+#define CSD_CONFIG_CurrentLsb20mA  	200
+/*Calibration Calculation parameter*/
+
+
 
 
 /* CSD203 Regsistor Config*/
@@ -37,6 +59,7 @@
 #define CSD_CONFIG_ShuntBus_CON 7
 
 #define CSD_CONFIG_ADDR_A1_GND_A2_GND 64
+
 typedef struct{
 	uint8_t DeviceADDR;
 	uint8_t RST;
@@ -44,6 +67,8 @@ typedef struct{
 	uint8_t VBUS_Conv_Time;
 	uint8_t VShunt_Conv_Time;
 	uint8_t Mode;
+	uint16_t CurrentLSB;
+	uint16_t Rshunt;
 }CSD_CONFIG;
 /*Initial CSD203 Config*/
 void CSD203_Init(CSD_CONFIG *CSD203_CFG);
@@ -54,6 +79,6 @@ uint16_t CSD203_ReadRshunt(CSD_CONFIG *CSD203_CFG);
 /*Read Power*/
 uint16_t CSD203_ReadPower(CSD_CONFIG *CSD203_CFG);
 /*Read Current*/
-uint16_t CSD203_ReadCurrent(CSD_CONFIG *CSD203_CFG)
+uint16_t CSD203_ReadCurrent(CSD_CONFIG *CSD203_CFG);
 
 #endif 
