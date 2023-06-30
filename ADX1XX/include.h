6d6fea36cbf1175bd10c3dc548cb647869031ee8
xutong-analog <114386539+xutong-analog@users.noreply.github.com>
2023-06-30 18:02:41 +0800

    Create include.h

---
+++ ADX1XX/include.h
@@ -0,0 +1,22 @@
+#ifndef _INCLUDE_H_
+#define _INCLUDE_H_
+
+#define __INT64 __int64
+
+#define NULL 0 
+
+typedef   signed          char int8_t;
+typedef   signed short     int int16_t;
+typedef   signed           int int32_t;
+typedef   signed       __INT64 int64_t;
+
+    /* exact-width unsigned integer types */
+typedef unsigned          char uint8_t;
+typedef unsigned short     int uint16_t;
+typedef unsigned           int uint32_t;
+typedef unsigned       __INT64 uint64_t;
+
+
+
+
+#endif
